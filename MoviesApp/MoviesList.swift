d0d70eff6cf1ab48fec1164ac1a58657ccd568fc
baralrojan <31406771+baralrojan@users.noreply.github.com>
2022-12-05 19:03:23 -0500

    All functionalty checked

--- MoviesApp/MoviesList.swift
+++ MoviesApp/MoviesList.swift
@@ -11,13 +11,12 @@ class MoviesList{
     var allMovies = [Movies]()
     
     init(){
-        var movieNames = ["Eternals","Dune","No Time To Die","Last Night in Soho","Ron's Done Wrong","Halloween Kills","Venom","Antlers","The Addams Family 2"]
-        
-        for _ in  1...10{
-            let movieNameIndex = Int.random(in: 0..<movieNames.count)
-            
-            allMovies.append(Movies(movieName: movieNames[movieNameIndex]))
+        let movieNames = ["Eternals","Dune","No Time To Die","Last Night in Soho","Ron's Done Wrong","Halloween Kills","Venom","Antlers","The Addams Family 2"]
+        let names_with_index = movieNames.enumerated()
+        for (index, name) in names_with_index {
+            allMovies.append(Movies(movieName: movieNames[index]))
         }
+   
     }
     func deleteMovies(index: Int){
         allMovies.remove(at: index)
